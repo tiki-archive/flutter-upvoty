94bc06585a0d798a072638149d18dd38e36a73c8
Ricardo Gonçalves <ricardo@mytiki.com>
2022-05-10 16:31:56 -0300

    update buttons sizing

--- lib/ui/button.dart
+++ lib/ui/button.dart
@@ -21,37 +21,36 @@ class UpvotyWidgetButton extends StatelessWidget {
     return GestureDetector(
         onTap: () => _service.presenter.showModal(context, _title, _boardHash),
         child: Container(
-            padding: EdgeInsets.symmetric(
-                vertical: SizeProvider.instance.size(17),
-                horizontal: SizeProvider.instance.size(17)),
+            padding: EdgeInsets.only(
+                top: SizeProvider.instance.width(18),
+                bottom: SizeProvider.instance.width(18),
+                left: SizeProvider.instance.width(18),
+                right: SizeProvider.instance.width(18)),
             decoration: BoxDecoration(
-              color: ColorProvider.greyTwo,
-              borderRadius: BorderRadius.circular(SizeProvider.instance.size(12)),
+              color: const Color(0xFFF0F0F0),
+              borderRadius: BorderRadius.circular(12),
             ),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Icon(
-                    _icon,
-                    color: _color,
-                    size: SizeProvider.instance.text(24),
-                  ),
+                  Icon(_icon,
+                      color: _color,
+                      size: SizeProvider.instance.width(24)),
                   Padding(
-                      padding: EdgeInsets.only(right: SizeProvider.instance.size(10))),
+                      padding: EdgeInsets.only(
+                          right: SizeProvider.instance.width(14))),
                   Expanded(
                       child: Text(_title,
                           style: TextStyle(
-                              fontSize: SizeProvider.instance.text(12),
                               fontFamily: TextProvider.familyNunitoSans,
                               package: 'tiki_style',
                               color: _color,
+                              fontSize: SizeProvider.instance.text(16),
                               fontWeight: FontWeight.bold))),
-                  Icon(
-                    IconProvider.arrow_right,
-                    color: _color,
-                    size: SizeProvider.instance.text(16),
-                  )
+                  Icon(IconProvider.arrow_right,
+                      color: _color,
+                      size: SizeProvider.instance.width(16)),
                 ])));
   }
 }
